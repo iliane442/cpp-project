dc274ec8eb8fc1c63df674745a6c12d313cf5924
Iliane <76448509+iliane442@users.noreply.github.com>
2025-04-11 10:28:08 +0200

    Update main.cpp
    test de arbre

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 //main.cpp
-#include "Roue.h"
-#include "Arbre.h"
+#include "Trans.h"
 
 void main(){
-    //construction Roue
-    Roue roue1;
-    
+    //construction
+	double vitesse = 340, m_menante = 20, m_menee = 15;
+	int nb_dents_menante = 20, nb_dents_menee = 15;
+	bool sens = 1;
+    Roue roue_menante = Roue(m_menante,nb_dents_menante);
+	Roue roue_menee = Roue (m_menee,nb_dents_menee);
+    Arbre arbre_1 = Arbre(&roue_menante,&roue_menee,vitesse,sens); 
 }
